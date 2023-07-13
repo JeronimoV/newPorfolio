51b730382af2d5f11c91e9252cf0c31993065519
bobardou <jeronimoevilar@outlook.com>
2023-07-13 16:31:46 -0300

    fix

--- src/components/presentation/presentation.jsx
+++ src/components/presentation/presentation.jsx
@@ -3,18 +3,20 @@ import styles from "./presentation.module.css";
 const Presentation = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.secondContainer}>
-        <div className={styles.textContainer}>
+      <div className={styles.header}>
+        <div className={styles.description}>
           <h1>Jeronimo Vilar</h1>
-          <h2>Full Stack Web Developer</h2>
-          <div className={styles.button}>
-              <p className={styles.buttonTitle}>Touch to expand!</p>
-              <p className={styles.hiddenButtonTitle}>Sobre mi</p>
-              <p className={styles.buttonText}>Hola, soy Jeronimo Vilar, soy de Argentina, Buenos Aires, soy Desarrollador Web FullStack con 20 años.
-  Me encanta programar, mi sueño es encontrar un trabajo para seguir aprendiendo. Mi objetivo ahora es encontrar un trabajo para ganar experiencia, ¡quiero ser un desarrollador sénior algún día!</p>
-          </div>
+          <h2>FullStack Web Developer</h2>
         </div>
-        <img className={styles.image} src="/foto.png" />
+        <img src="./fotoCaraAmarilla.png"/>
+      </div>
+      <div className={styles.buttonContainer}>
+        <p className={styles.button}>Click to expand!</p>
+        <div className={styles.hiddenText}>
+          <p className={styles.title}>Sobre Mi</p>
+          <p className={styles.text}>Hola, soy Jeronimo Vilar, soy de Argentina, Buenos Aires, soy Desarrollador Web FullStack con 20 años.
+          Me encanta programar, mi sueño es encontrar un trabajo para seguir aprendiendo. Mi objetivo ahora es encontrar un trabajo para ganar experiencia, ¡quiero ser un desarrollador sénior algún día!</p>
+        </div>
       </div>
     </div>
   );
